646cb550303ce750c5d9c1bc4163fb6a7a23e402
LHJ Piper <2940596912@qq.com>
2019-12-10 23:58:17 +0800

    init & k-armed with greedy

---
+++ README.md
@@ -0,0 +1,4 @@
+<p align="center">
+![](https://img.shields.io/badge/公众号-PiperNest-orange.svg)
+</p>
+
---
+++ open_lRLwp_jupyter.bat
@@ -0,0 +1,4 @@
+CALL D:\Anaconda3\Scripts\activate.bat D:\Anaconda3
+d:
+cd D:\GitHub\Reinforcement-Learning-practice-zh
+jupyter notebook
